af8413dca15ef046c4e14e3fb5bb7f94c654e2e9
Fadli <fadlidzilikram@gmail.com>
2026-01-28 23:55:37 +0700

    refactor: move credentials to separate folder for n8n scanner compatibility

---
+++ credentials/MailketingApi.credentials.ts
@@ -0,0 +1,17 @@
+import { ICredentialType, INodeProperties } from 'n8n-workflow';
+
+export class MailketingApi implements ICredentialType {
+    name = 'mailketingApi';
+    displayName = 'Mailketing API';
+
+    properties: INodeProperties[] = [
+        {
+            displayName: 'API Token',
+            name: 'apiToken',
+            type: 'string',
+            default: '',
+            required: true,
+            description: 'API Token dari menu Integration Mailketing',
+        },
+    ];
+}
